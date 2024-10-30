2bd56e9f38988842bb4652ad7c24851e3b51b0b8
David <davidopotowsky14@gmail.com>
2024-10-30 17:41:36 -0500

    drafted listItemsSlice

---
+++ src/features/listItems/ListItems.jsx
@@ -0,0 +1,68 @@
+// import { useSelector } from "react-redux";
+// import {
+//   useDeleteListMutation,
+//   useGetListQuery,
+// } from "./listSlice";
+// import { useParams, Navigate } from "react-router-dom";
+// import { useState } from "react";
+// import UpdateListForm from "./UpdateListForm";
+// import "./listDetails.css";
+
+
+//need to get ListItems to render onto the individual list page via ListDetails from ListItems
+// import React from "react"; 
+// import {userParams} from "react-router-dom"
+ 
+// const ListItems = 
+
+import { useState } from "react";
+import ListDetails from "./ListDetails";
+import { useGetListQuery } from "./listSlice";
+import { useSelector } from "react-redux";
+import { selectToken } from "../auth/authSlice";
+import { Link, useNavigate } from "react-router-dom";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// return (
+//     <article>
+//       <h1>Departments</h1>
+//       <div className="departmentGroup">
+//         <div className="listOfDepartments">
+//           <ul className="departments">
+//             {isLoading && <li>Loading departments...</li>}
+//             {departments.map((department) => (
+//               <li key={department.id}>
+//                 <h2>{department.name}</h2>
+//                 <figure>
+//                   <h3>{department.description}</h3>
+//                   <h2>{department.email}</h2>
+//                   <h2>{department.phone}</h2>
+//                   <img src={department.imageUrl} alt={department.name} />
+//                 </figure>
+//                 <button onClick={() => handleSeeDetails(department.id)}>
+//                   See Details
+//                 </button>
+//               </li>
+//             ))}
+//           </ul>
+//         </div>
+//         <div className="departmentForm">
+//           <DepartmentForm />
+//         </div>
+//       </div>
+//     </article>
+//   );
+
+export default ListItems
