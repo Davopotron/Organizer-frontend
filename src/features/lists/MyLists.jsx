653be3dd21054c5d66ab1828fac351b2c4cdf81d
Alexis Sushko <alexis.sushko@gmail.com>
2024-10-30 13:56:52 -0400

    Updated MyList.jsx to map over individual lists

--- src/features/lists/MyLists.jsx
+++ src/features/lists/MyLists.jsx
@@ -1,17 +1,18 @@
-import {useState} from 'react';
-import ListDetails from './ListDetails';
-// import {useGetListQuery} from '../../store/facultySlice';
-import {useSelector} from 'react-redux';
+import { useState } from "react";
+import ListDetails from "./ListDetails";
+import { useGetListQuery } from "";
+import { useSelector } from "react-redux";
 // import {selectToken} from '../../store/authSlice';
-import {Link} from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
+import AddListForm from "./AddListForm";
 
 // Function that renders a list of all lists
 export default function ItemsList() {
-  const {data: items, error, isLoading} = useGetFacultyQuery();
+  const { data: myLists = [], isLoading, error } = useGetListQuery();
   const token = useSelector(selectToken);
-
-  const [filter, setFilter] = useState('');
-  const searchRegex = new RegExp(filter, 'i');
+  const navigate = useNavigate();
+  //const [filter, setFilter] = useState('');
+  //const searchRegex = new RegExp(filter, 'i');
 
   if (isLoading) return <h2>Loading List...</h2>;
 
@@ -24,6 +25,33 @@ export default function ItemsList() {
   }
 
   return (
+    <article>
+      <h1>My Lists</h1>
+      <div className="listGroup">
+        <div className="listOfLists">
+          <ul className="lists">
+            {isLoading && <li>Loading lists...</li>}
+            {lists.map((list) => (
+              <li key={list.id}>
+                <h2>{list.name}</h2>
+                <figure>
+                  <h3>{list.description}</h3>
+                </figure>
+                <button onClick={() => handleSeeDetails(list.id)}>
+                  See Details
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="listForm">
+          <AddListForm />
+        </div>
+      </div>
+    </article>
+  );
+  /*
+  return (
     <main>
       <h1>List</h1>
       <form>
@@ -35,7 +63,7 @@ export default function ItemsList() {
       </form>
       {token && <Link to={`/list/form`}>Create a new item</Link>}
       <ul className='listlists'>
-        {professors
+        {lists
           .filter((items) => item.name.match(searchRegex))
           .sort((a, z) => a.name.localeCompare(z.name))
           .map((item) => (
@@ -43,4 +71,5 @@ export default function ItemsList() {
       </ul>
     </main>
   );
+   */
 }
