cf369a5a20386e53747ab20fb145ce307ec34c4a
Alexis Sushko <alexis.sushko@gmail.com>
2024-10-30 13:18:21 -0400

    Deleted extra router folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 import router from "./router.jsx";
 import store from "./app/store.js";
 import { Provider } from "react-redux";
--- src/router.jsx
+++ src/router.jsx
@@ -2,9 +2,10 @@ import { createBrowserRouter } from "react-router-dom";
 import AuthForm from "./features/auth/AuthForm";
 import Root from "./layout/Root";
 import HomePage from "./features/HomePage";
+import Login from "./features/auth/AuthForm";
 //import AddListForm from "./features/lists/AddListForm";
 //import ListDetails from "./features/lists/ListDetails";
-//import MyLists from "./features/lists/MyLists";
+import MyLists from "./features/lists/MyLists";
 //import UpdateListForm from "./features/lists/UpdateListForm";
 //import AddListItemForm from "./features/listItems/AddListItemForm";
 //import ListItems from "./features/listItems/ListItems";
@@ -16,9 +17,9 @@ const router = createBrowserRouter([
     element: <Root />,
     children: [
       { index: true, element: <HomePage />, path: "/" },
-      //{ path: "/auth", element: <AuthForm /> },
-      //{ path: "/login", element: <AuthForm /> },
-      //{ path: "/MyList", element: <MyLists /> },
+      { path: "/auth", element: <AuthForm /> },
+      { path: "/users/login", element: <Login /> },
+      { path: "/MyList", element: <MyLists /> },
       //{ path: "/MyList/:id", element: <ListDetails /> },
       //{ path: "/MyList", element: <AddListForm /> },
       //{ path: "/MyList/:id", element: <UpdateListForm /> },
