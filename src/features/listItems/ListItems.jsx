737f1c15996cfd21c684c0ab3baf21c29f8d9167
David <davidopotowsky14@gmail.com>
2024-10-30 18:08:58 -0500

    drafting ListItems.jsx

--- src/features/listItems/ListItems.jsx
+++ src/features/listItems/ListItems.jsx
@@ -17,10 +17,26 @@
 
 import { useState } from "react";
 import ListDetails from "./ListDetails";
-import { useGetListQuery } from "./listSlice";
+import { useGetListItemsQuery } from "./listItemsSlice";
 import { useSelector } from "react-redux";
 import { selectToken } from "../auth/authSlice";
 import { Link, useNavigate } from "react-router-dom";
+
+//Function that renders a singular list's items
+
+export default function ItemRender() {
+    const { data: listItem = [], isLoading, error } = useGetListItemsQuery();
+    const token = useSelector(selectToken);
+    const navigate = useNavigate();
+
+    if (isLoading) return <h2>Loading Item</h2>;
+
+    if (error) {
+        return <p>{error.message}</p>;
+    }
+
+    if (!)
+}
 
 
 
@@ -65,4 +81,4 @@ import { Link, useNavigate } from "react-router-dom";
 //     </article>
 //   );
 
-export default ListItems
+// export default ListItems
