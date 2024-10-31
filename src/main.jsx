5c9ca1ffe4577a549602ebbe42fc8841c6a7b49d
Marina <marvarado19@gmail.com>
2024-10-31 11:39:54 -0700

    my list details buttons

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import router from "./router.jsx";
-import store from "./app/store.js";
-import { Provider } from "react-redux";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {RouterProvider} from 'react-router-dom';
+import router from './router.jsx';
+import store from './app/store.js';
+import {Provider} from 'react-redux';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <RouterProvider router={router} />
