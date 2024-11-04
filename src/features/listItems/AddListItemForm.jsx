9958385f384715871a0a60234967a6bc81c068da
Alexis Sushko <alexis.sushko@gmail.com>
2024-11-04 14:58:32 -0500

    Updated add list item form

--- src/features/listItems/AddListItemForm.jsx
+++ src/features/listItems/AddListItemForm.jsx
@@ -1,33 +1,33 @@
-
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { useAddListItemsMutation } from "./listItemsSlice";
 
-export default function AddListItemForm() {
-//   const [listItemData, setListItemData] = useState(""); -comment
-const [ itemName, setItemName ] = useState("");
-const [myListId, setMyListId] = useState(1);
+export default function AddListItemForm({ myListId }) {
+  //   const [listItemData, setListItemData] = useState(""); -comment
+  const [itemName, setItemName] = useState("");
+  // const [myListId, setMyListId] = useState(null);
 
   //const navigate = useNavigate(); -comment
-  const [addListItem, { isLoading: isAdding, error: addingError }] = useAddListItemsMutation();
+  const [addListItem, { isLoading: isAdding, error: addingError }] =
+    useAddListItemsMutation();
 
-//   const postListItem = async (event) => { -comment
-//     event.preventDefault(); -comment
-//     try { -comment
-//       /*const listItem = */ await addListItem({ -comment
-//         ...listItemData, -comment
-//       }).unwrap(); -comment
-//     } catch (e) { -comment
-//       console.error(e); -comment
-//     }
-//   };
+  //   const postListItem = async (event) => { -comment
+  //     event.preventDefault(); -comment
+  //     try { -comment
+  //       /*const listItem = */ await addListItem({ -comment
+  //         ...listItemData, -comment
+  //       }).unwrap(); -comment
+  //     } catch (e) { -comment
+  //       console.error(e); -comment
+  //     }
+  //   };
 
-const postList = async (event) => {
+  const postList = async (event) => {
     event.preventDefault();
     const listData = {
       itemName,
       myListId,
-      };
+    };
     console.log("Posting list data:", listData);
     try {
       const response = await addListItem(listData).unwrap();
@@ -35,31 +35,29 @@ const postList = async (event) => {
     } catch (e) {
       console.error("Failed to add list:", e);
     }
-};
+  };
 
   return (
     <>
-    <h2>Add a List Item</h2>
-    <form onSubmit={postList}>
-      <div className="addListItem">
-        <label>
-          Item Name
-          {/* <input   -comment 
+      <h2>Add a List Item</h2>
+      <form onSubmit={postList}>
+        <div className="addListItem">
+          <label>
+            Item Name
+            {/* <input   -comment 
             name="itemName"   -comment
             value={listItemData.item}  -comment
             onChange={(e) => -comment
               setListItemData({ ...listItemData, item: e.target.value }) -comment
-            } */} 
-            <input 
-            id="itemName"
-            name="itemName"
-            value={itemName}
-            onChange={(e) =>
-              setItemName(e.target.value )
-            }
-          />
-        </label>
-        {/* <label>  -comment
+            } */}
+            <input
+              id="itemName"
+              name="itemName"
+              value={itemName}
+              onChange={(e) => setItemName(e.target.value)}
+            />
+          </label>
+          {/* <label>  -comment
           ListId  -comment
           <input -comment
             name="myListId"  -comment
@@ -69,11 +67,11 @@ const postList = async (event) => {
             } -comment
           /> -comment
         </label> */}
-      </div>
-      <button type="submit">Confirm Save</button>
-      {isAdding && <output>Uploading list item</output>}
-      {addingError && <output>{addingError.message}</output>}
-    </form>
+        </div>
+        <button type="submit">Confirm Save</button>
+        {isAdding && <output>Uploading list item</output>}
+        {addingError && <output>{addingError.message}</output>}
+      </form>
     </>
   );
 }
--- src/features/myLists/MyListDetails.jsx
+++ src/features/myLists/MyListDetails.jsx
@@ -1,34 +1,40 @@
-import React, { useState } from 'react';
-import {Link, useParams} from 'react-router-dom';
-import {useGetMyListQuery} from './myListsSlice';
-import { useGetListItemsQuery } from '../listItems/listItemsSlice';
-import { useUpdateListItemsMutation, useAddListItemsMutation, useDeleteListItemsMutation } from '../listItems/listItemsSlice';
-import ListItems from '../listItems/ListItems';
-import {useSelector} from 'react-redux';
-import AddListForm from './AddMyListForm';
-import AddListItemForm from '../listItems/AddListItemForm';
+import React, { useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { useGetMyListQuery } from "./myListsSlice";
+import { useGetListItemsQuery } from "../listItems/listItemsSlice";
+import {
+  useUpdateListItemsMutation,
+  useAddListItemsMutation,
+  useDeleteListItemsMutation,
+} from "../listItems/listItemsSlice";
+import ListItems from "../listItems/ListItems";
+import { useSelector } from "react-redux";
+import AddListForm from "./AddMyListForm";
+import AddListItemForm from "../listItems/AddListItemForm";
 // import {useGetProfessorQuery} from '../../store/facultySlice';
 
 // export default function Professor() {
 export default function ListDetails() {
-  const {id} = useParams();
-  const {data: myList, isLoading} = useGetMyListQuery(id);
-  const {data: listItems, isLoadingListItem} = useGetListItemsQuery()
+  const { id } = useParams();
+  const { data: myList, isLoading } = useGetMyListQuery(id);
+  const { data: listItems, isLoadingListItem } = useGetListItemsQuery();
   const [deleteListItem] = useDeleteListItemsMutation();
   const [updateListItem] = useUpdateListItemsMutation(id);
-  const [editMode, setEditMode] = useState('');
+  const [editMode, setEditMode] = useState("");
   const [newName, setNewName] = useState("");
   // const token = useSelector(selectToken);
   // const navigate = useNavigate();
 
   //console.log(list);
+
+  const listId = parseInt(id, 10);
   if (isLoading) return <p>Loading Item...</p>;
 
   const handleEdit = async (id, currentName) => {
     setEditMode(id);
     setNewName(currentName);
-    
-  //   console.log('Edit clicked');
+
+    //   console.log('Edit clicked');
     // const newName = ('Enter new list details:', myList.itemName);
     // const newDetail = ('Enter new detail:', myList.detail);
     // if (newName !== null && newDetail !== null) {
@@ -43,99 +49,104 @@ export default function ListDetails() {
   //   }
   // }
 
-
-
   const handleUpdate = async (id) => {
     if (newName.trim()) {
       await updateListItem({ id, itemName: newName });
       setEditMode(null);
       // setListItemId("");
-    };
+    }
   };
 
   const handleDelete = async (listItemId) => {
-  if (window.confirm("Are you sure you want to delete this list item?")){
-    try {
-      await deleteListItem(listItemId).unwrap();
-    } catch (error) {
-      console.error("failed to delete list item:", error);
+    if (window.confirm("Are you sure you want to delete this list item?")) {
+      try {
+        await deleteListItem(listItemId).unwrap();
+      } catch (error) {
+        console.error("failed to delete list item:", error);
+      }
     }
-  }
   };
 
   return (
     <>
-    <table>
-      <tbody>
-        <tr>
-          <th>
-           <h1>List Details</h1>
-          <h2>{myList.name}</h2>
-          {/* <button onClick={() => handleAdd(listItem.id)}>Add Item</button> */}
-          <div className="addForm">
-            <AddListItemForm />
-          </div>
-          {myList.listItems.map((listItem) => (
-            <li key={listItem.id}>
-              <h2>{listItem.itemName}</h2>
-              {editMode === listItem.id ? (
+      <table>
+        <tbody>
+          <tr>
+            <th>
+              <h1>List Details</h1>
+              <h2>{myList.name}</h2>
+              {/* <button onClick={() => handleAdd(listItem.id)}>Add Item</button> */}
+              <div className="addForm">
+                <AddListItemForm myListId={listId} />
+              </div>
+              {myList.listItems.map((listItem) => (
+                <li key={listItem.id}>
+                  <h2>{listItem.itemName}</h2>
+                  {editMode === listItem.id ? (
                     <>
                       <input
                         type="text"
                         value={newName || ""}
                         onChange={(e) => setNewName(e.target.value)}
                       />
-                      <button onClick={() => handleUpdate(listItem.id)}>Save</button>
+                      <button onClick={() => handleUpdate(listItem.id)}>
+                        Save
+                      </button>
                       <button onClick={() => setEditMode(null)}>Cancel</button>
                     </>
                   ) : (
                     <>
-                      <button onClick={() => handleEdit (listItem.id, listItem.name)}
-                      >Edit</button>
-                      <button onClick={() => handleDelete(listItem.id)}>Delete</button>
+                      <button
+                        onClick={() => handleEdit(listItem.id, listItem.name)}
+                      >
+                        Edit
+                      </button>
+                      <button onClick={() => handleDelete(listItem.id)}>
+                        Delete
+                      </button>
                     </>
                   )}
-            </li>
-          ))}
-        {/* </li>
+                </li>
+              ))}
+              {/* </li>
       </ul> */}
-      </th>
-      </tr>
-      </tbody>
+            </th>
+          </tr>
+        </tbody>
       </table>
     </>
   );
-};
+}
 
-  //   return (
-  //     <>
-  //       <h1>List Details</h1>
-  //       <ul>
-  //         <li>
-  //           {console.log(myList)}
-  //           <h2>{myList.name}</h2>
-  //           <p>
-  //             <b>Description:</b> {myList.details}
-  //           </p>
-  //           <p>
-  //             <b>Owner: </b>
-  //             {myList.ownerId}
-  //           </p>
-  //           <p>
-  //             <b>Items: </b>
-  //             {myList.length}
-  //           </p>
-  //           <ListDetails />
-  //           <div>
-  //             <button onClick={() => handleEdit()}>Edit List</button>
-  //             <button onClick={() => handleDelete()}>Delete</button>
-  //           </div>
-  //           <div>
-  //             {myList.items.map((item, index) => (
-  //               <p key={index}>{myList.name}</p>
-  //             ))}
-  //           </div>
-  //         </li>
-  //       </ul>
-  //     </>
-  //   );
+//   return (
+//     <>
+//       <h1>List Details</h1>
+//       <ul>
+//         <li>
+//           {console.log(myList)}
+//           <h2>{myList.name}</h2>
+//           <p>
+//             <b>Description:</b> {myList.details}
+//           </p>
+//           <p>
+//             <b>Owner: </b>
+//             {myList.ownerId}
+//           </p>
+//           <p>
+//             <b>Items: </b>
+//             {myList.length}
+//           </p>
+//           <ListDetails />
+//           <div>
+//             <button onClick={() => handleEdit()}>Edit List</button>
+//             <button onClick={() => handleDelete()}>Delete</button>
+//           </div>
+//           <div>
+//             {myList.items.map((item, index) => (
+//               <p key={index}>{myList.name}</p>
+//             ))}
+//           </div>
+//         </li>
+//       </ul>
+//     </>
+//   );
